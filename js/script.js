eb4dd59fa7515d9d3b3a738b1eb695861d4aca09
Gabri1391 <chiarellogabriele@gmail.com>
2022-06-29 16:38:37 +0200

    push

--- js/script.js
+++ js/script.js
@@ -4,26 +4,61 @@
 //-Capovolgo la parola
 //- if per controllare se le due parole sono palindrome
 
-const userWord = prompt('Inerisci una parola').trim();
-console.log(`La parola inserita è: ${userWord}`);
 
-function reverseWord(userWord){
-    let result = '';
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+//PARI E DISPARI
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+//Dichiariamo chi ha vinto.
+
+const evenOdd = prompt('Pari o Dispari?','Pari');
+console.log(evenOdd);
+
+const min = 1;
+const max = 5;
+
+//CHIEDIAMO IL NUMERO ALL'UTENTE
+const userNumber = parseInt(prompt(`Scegli un numero compreso tra ${min} e ${max}`));
+console.log(userNumber);
+
+
+//FUNZIONE PER GENERARE UN NUMERO RANDOM ALLA CPU
+function getCpuRandomNumber(){
     
-    for(i = userWord.length - 1; i >= 0 ; i--){
-        result += userWord[i];
-       
+    const cpuRandomNumber = Math.floor(Math.random() * (max + 1));
+
+    return cpuRandomNumber;
+}
+
+ const cpuNumber = getCpuRandomNumber();
+ console.log(cpuNumber);
+
+
+ //SOMMA DEI DUE NUMERI
+ const sum = userNumber + cpuNumber;
+ console.log(sum);
+
+
+ //FUNZIONE PER STABILIRE SE  LA SOMMA DEI DUE RISULTATI SIA PARI O DISPARI
+
+ function isEven(number){
+    let result;
+
+    if(number % 2){
+        result = false;
+    }else{
+        result = true;
     }
 
+
     return result;
-}
+ }
 
-const reversedWord = reverseWord(userWord);
-console.log(`La parola al contrario é: ${reversedWord}`);
-
-if(userWord === reversedWord){
-
-    console.log('La due parole sono palindrome');
-}else{
-    console.log('Le due parole non sono palindrome');
-}
+ const finalResult = isEven(sum); 
+ console.log(finalResult);
